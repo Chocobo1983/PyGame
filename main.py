b87630340cd0fbccaa58cd009077c524520bab50
stb.infoserv <stb.infoserv@gmail.com>
2023-06-23 16:36:34 +0300

    added movement

--- main.py
+++ main.py
@@ -36,14 +36,47 @@ player_anim_count = 0
 
 bg_x = 0
 
+player_speed = 5
+player_x = 300
+player_y = 310
+
+is_Jump = False
+jump_count = 7
+
 bg_sound = pygame.mixer.Sound('resources/sounds/bg.mp3')
-bg_sound.play()
+#bg_sound.play()
 
 while running:
 
     screen.blit(bg, (bg_x,0))
     screen.blit(bg, (bg_x+800,0))
-    screen.blit(walk_right[player_anim_count], (300,310))
+    screen.blit(walk_right[player_anim_count], (player_x,player_y))
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        screen.blit(walk_left[player_anim_count], (player_x,player_y))
+    else:
+        screen.blit(walk_right[player_anim_count], (player_x,player_y))
+
+    if not is_Jump:
+        if keys[pygame.K_SPACE]:
+            is_Jump = True
+    else:
+        if jump_count >= -7:
+            if jump_count > 0:
+                player_y -=  (jump_count**2)/2
+            else:
+                player_y += (jump_count**2)/2
+            jump_count -=1
+        else:
+            is_Jump = False
+            jump_count = 7
+
+    if keys[pygame.K_LEFT] and player_x > 50:
+        player_x -= player_speed
+    elif keys[pygame.K_RIGHT] and player_x < 750:
+        player_x += player_speed
 
     if player_anim_count == 3: 
         player_anim_count = 0
